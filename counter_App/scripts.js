adc1a250a36fcd317bcf98cc6e5e22150e8eb6d2
Ibeawuchi Sopuru George <platoon1277@gmail.com>
2022-09-08 03:57:47 +0100

    javascript-updated

--- counter_App/scripts.js
+++ counter_App/scripts.js
@@ -1,1 +1,17 @@
-console.log("food")
+// 1. create a variable that initialize to 0
+// 2. create a function that when called will increase the initialized variable as well as store it.
+// 3. create another funtion that will save the varible when clicked
+// 4. Add necessary DoMs with the htmls
+
+let count = 0;
+let saved = document.getElementById("saved-el")
+function increase() {
+    count +=  1
+    document.getElementById("counter-el").innerText = count;
+    
+};
+function save() {
+    countED = count + "-";
+    saved.innerText += countED;
+    
+}
