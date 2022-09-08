64f159ac8e3b6f1a5ace7362387d9643013a9ac4
Ibeawuchi Sopuru George <platoon1277@gmail.com>
2022-09-08 04:10:47 +0100

    javascript-updated

--- counter_App/scripts.js
+++ counter_App/scripts.js
@@ -2,6 +2,7 @@
 // 2. create a function that when called will increase the initialized variable as well as store it.
 // 3. create another funtion that will save the varible when clicked
 // 4. Add necessary DoMs with the htmls
+// 5. Add another function that restart the counter after saved to 0.
 
 let count = 0;
 let saved = document.getElementById("saved-el")
@@ -11,7 +12,12 @@ function increase() {
     
 };
 function save() {
-    countED = count + "-";
+    countED = count + " - ";
     saved.innerText += countED;
+    
+}
+function restart() {
+    count = 0;
+    document.getElementById("counter-el").innerText = count;
     
 }
