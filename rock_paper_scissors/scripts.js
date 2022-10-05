e33d3c9af936008438a105c9e371ce4c3d8116b2
Ibeawuchi Sopuru George <platoon1277@gmail.com>
2022-10-05 08:01:23 +0100

    Rock,paper,scissors text run game

--- rock_paper_scissors/scripts.js
+++ rock_paper_scissors/scripts.js
@@ -1,20 +1,48 @@
 // create a function getComputer, that will return either rock,scissors or paper;
 // the proposed returned value will be stored in a variable.
 
+const options = ["Rock","Paper","Scissors",]
 
-let game1 = "Rock";
-let game2 =  "Paper";
-let game3 =  "scissors";
+function getComputerChoice() {
+   const choice = options[Math.floor(Math.random() * options.length)];
+   return choice; 
+};
+console.log(getComputerChoice())
 
-function getComputer(game) {
-    if (game == "R") {
-        return game1;
-    }if (game == "P") {
-        return game2;
-    }if (game == "S"){
-        return game3;
-    }else {
-        return null;       
+// 1. Write a function that plays a single round of Rock Paper Scissors
+// 2. The function should take two parameters - the playerSelection and computerSelection
+// 3. And then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
+
+// let round1 = "You Lose! Paper beats Rock"
+// let round2 = "You Lose! Scissors beats Paper"
+// let round3 = "You Lose! Rock beats Scissors"
+
+    function checkWinner(playerSelection, computerSelection) {
+            if (playerSelection === computerSelection) {
+                return "Tie";
+                    } else if (
+                      (playerSelection === "Rock" && computerSelection === "Scissors") ||
+                        (playerSelection === "Paper" && computerSelection === "Rock")||
+                            (playerSelection === "Scissors" && computerSelection === "Paper"))
+         {
+                         return "player";
+                     }else{
+                        return "computer";
+                     }   
+};
+   function playRound(playerSelection, computerSelection){
+    const result = checkWinner(playerSelection, computerSelection);
+    if (result == "Tie"){
+        return "viola!, its a Tie";
+    } else if (result == "player"){
+        return `you win, ${playerSelection} wins ${computerSelection}`
+    }else{
+        return `you lose, ${computerSelection} beats ${playerSelection}`;
     }
 };
-console.log(getComputer("S"));
+
+const playerSelection = prompt("enter option, "); /**the aim of this comment is to test run*/
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection))
+
+
